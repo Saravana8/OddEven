f34e66a65bacf8db13e9affc4461f52587b1daf1
Saravana8 <saravanak49@gmail.com>
2019-10-18 17:06:47 +0530

    OddEven code

---
+++ src/org/odd/OddEven.java
@@ -0,0 +1,17 @@
+package org.odd;
+
+public class OddEven {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(sum);//notes
+			    sum=sum+i;
+			}
+			    
+	}
+
+}
+}
---
+++ src/org/oddeven/OddEve.java
@@ -0,0 +1,11 @@
+package org.oddeven;
+
+public class OddEve {
+	 public static void main(String[] args) {
+		    for (int i = 1; i <= 100; i++) {
+		    	if(i==99)
+		      System.out.println(i+"");
+		    }
+
+		  }
+}
